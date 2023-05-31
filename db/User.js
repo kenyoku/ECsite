b429baa1127991b737ccd922f46113c7c37004a9
Yokuken <torunp850409@gmail.com>
2023-05-31 15:13:29 +0900

    dbfile

---
+++ db/User.js
@@ -0,0 +1,22 @@
+
+
+const User = [
+    {
+      email: "ken@gmail.com",
+      password: "0000",
+    },
+  ];
+  
+  const Register = [
+    {
+      email: "ken@gmail.com",
+      hashedPassword: "0000",
+    },
+  ];
+
+module.exports={
+
+    UserData:User,
+    RegisterData:Register,
+
+}
